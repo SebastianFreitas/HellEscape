384c1d9f2eec40fca3e9826f99c2a9db495dc2e3
ElTraffy <traffydeal@gmail.com>
2020-11-25 00:16:07 +0000

    all lights random

--- Assets/LightFlicker.cs
+++ Assets/LightFlicker.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LightFlicker : MonoBehaviour
 {
-    public GameObject light;
+    public List<GameObject> allObjects;
     public float timeOff;
     public float timeOn;
     // Start is called before the first frame update
@@ -12,28 +12,32 @@ public class LightFlicker : MonoBehaviour
 
     void Start()
     {
-        StartCoroutine(waiterOn(timeOn));
+        for(int i = 0; i<allObjects.Count;i++)
+        {
+            if(Random.value > 0.75) StartCoroutine(waiterOn(timeOn, i));
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    private IEnumerator waiterOn(float time)
+    private IEnumerator waiterOn(float time, int i)
     {
         time += Random.Range(-10f,10f);
         yield return new WaitForSeconds(time);
-        light.GetComponent<Light>().enabled = false;
-        StartCoroutine(waiterOff(timeOff));
+        allObjects[i].GetComponent<Light>().enabled = false;
+        StartCoroutine(waiterOff(timeOff, i));
     }
 
-    private IEnumerator waiterOff(float time)
-    {   
+    private IEnumerator waiterOff(float time,int i)
+    {
         time += Random.Range(-.1f,.1f);
         yield return new WaitForSeconds(time);
-        light.GetComponent<Light>().enabled = true;
-        StartCoroutine(waiterOn(timeOn));
+        allObjects[i].GetComponent<Light>().enabled = true;
+        StartCoroutine(waiterOn(timeOn, i));
     }
 }
