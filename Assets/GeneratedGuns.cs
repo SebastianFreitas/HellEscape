0ed6e7195c657ffeb59fb00f0a58dce83548f106
ElTraffy <traffydeal@gmail.com>
2021-09-28 19:09:47 +0100

    Update GeneratedGuns.cs

--- Assets/GeneratedGuns.cs
+++ Assets/GeneratedGuns.cs
@@ -52,7 +52,6 @@ public class GeneratedGuns : MonoBehaviour
             var currentGun = layoutSlots[i];
             if (currentGun != null)
             {
-                Debug.Log("enfiou");
                 selectX.gun = currentGun;
                 selectX.gunTypeText.text = currentGun.type.ToString();
             }
