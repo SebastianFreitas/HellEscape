55d57ec8cc334b0586dd2a88aa64c8f69067f6da
ElTraffy <traffydeal@gmail.com>
2022-04-16 13:23:50 +0100

    monkahmm bullets

--- Assets/JumpUp.cs
+++ Assets/JumpUp.cs
@@ -4,20 +4,12 @@ using UnityEngine;
 
 public class JumpUp : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Dude"))
-        {
-            collision.gameObject.GetComponent<PlayerBasicMovement>().AddImpact(Vector3.up, 5000f);
-            collision.gameObject.GetComponent<PlayerHpManager>().TakeDamage(5);
-        }
-    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Dude"))
         {
-            other.GetComponent<PlayerBasicMovement>().AddImpact(Vector3.up, 2500f);
+            other.GetComponent<PlayerBasicMovement>().AddImpact(Vector3.up, 1500f);
             other.GetComponent<PlayerHpManager>().TakeDamage(5);
         }
     }
