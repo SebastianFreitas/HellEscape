f00a1527122f9feb802df488d81e8c7cf3a7a5ac
ElTraffy <traffydeal@gmail.com>
2022-04-16 02:38:36 +0100

    jump path

--- Assets/JumpUp.cs
+++ Assets/JumpUp.cs
@@ -8,8 +8,17 @@ public class JumpUp : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Dude"))
         {
-            collision.gameObject.GetComponent<PlayerBasicMovement>().AddImpact(Vector3.up, 500f);
+            collision.gameObject.GetComponent<PlayerBasicMovement>().AddImpact(Vector3.up, 5000f);
             collision.gameObject.GetComponent<PlayerHpManager>().TakeDamage(5);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Dude"))
+        {
+            other.GetComponent<PlayerBasicMovement>().AddImpact(Vector3.up, 1000f);
+            other.GetComponent<PlayerHpManager>().TakeDamage(5);
         }
     }
 }
