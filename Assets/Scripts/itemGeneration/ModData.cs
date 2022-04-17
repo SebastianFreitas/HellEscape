c50fd205881a1cb91c2c3ac91aee655321bb3a5c
ElTraffy <traffydeal@gmail.com>
2022-04-17 23:08:01 +0100

    Update ModData.cs

--- Assets/Scripts/itemGeneration/ModData.cs
+++ Assets/Scripts/itemGeneration/ModData.cs
@@ -35,8 +35,13 @@ public class ModData : MonoBehaviour
 
                 new Mod(4,10,  "Shoot Horizontal Slashes",           Grade.special, OperatorType.non, 0,9),
                 new Mod(4,10,  "Slow Bullets",           Grade.special, OperatorType.non, 0,9),
-                new Mod(4,10,  "Shoot Horizontal Slashes",           Grade.special, OperatorType.non, 0,9),
-                new Mod(4,10,  "Shoot Horizontal Slashes",           Grade.special, OperatorType.non, 0,9)
+                new Mod(4,10,  "Delayed Shot",           Grade.special, OperatorType.non, 0,9),
+                new Mod(4,10,  "double shot",           Grade.special, OperatorType.non, 0,9),
+                new Mod(4,10,  "triple shot",           Grade.special, OperatorType.non, 0,9),
+                new Mod(4,10,  "Circle",           Grade.special, OperatorType.non, 0,9),
+                new Mod(4,10,  "",           Grade.special, OperatorType.non, 0,9),
+                new Mod(4,10,  "",           Grade.special, OperatorType.non, 0,9),
+                new Mod(4,10,  "",           Grade.special, OperatorType.non, 0,9)
 
         };
 
@@ -45,7 +50,7 @@ public class ModData : MonoBehaviour
 
     public static int[] ExteriorWeight = { 3, 3, 1, 3, 3};  //
 
-    public static int[] SpecialWeight = { 10, 10, 10, 10, 20,         1,1,1,1};
+    public static int[] SpecialWeight = { 10, 10, 10, 10, 20, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
 
     public static int[] modWeight = { 1000, 900, 800, 700, 600, 500, 400, 300, 200, 100 };
 
